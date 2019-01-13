4558f399f3cfd4bd480aa0910f159c18a30de6ac
flukycoder <flukycoder@gmail.com>
2019-01-13 21:47:19 +0530

    [POSt /todos Added]

--- server/server.js
+++ server/server.js
@@ -4,3 +4,29 @@ let bodyParser = require("body-parser");
 let { mongoose } = require("./db/mongoose");
 let { Todo } = require("./modals/todo");
 let { User } = require("./modals/user");
+
+let app = express();
+
+app.listen(3000, () => {
+  console.log("Started On Port 3000");
+});
+
+//setting up route
+app.use(bodyParser.json());
+app.post("/todos", (req, res) => {
+  let todo = new Todo({
+    text: req.body.text
+  });
+  todo.save().then(
+    doc => {
+      console.log("SAVED");
+      res.send(doc);
+    },
+    err => {
+      console.log("error");
+      res.status(400).send(err);
+    }
+  );
+
+  console.log(req.body);
+});
