51d440c9d1d5f9ad2345e9f98d0013c6d0ce4178
flukycoder <flukycoder@gmail.com>
2019-01-13 22:18:07 +0530

    [MERGE]

--- server/server.js
+++ server/server.js
@@ -30,3 +30,14 @@ app.post("/todos", (req, res) => {
 
   console.log(req.body);
 });
+
+app.get("/todos", (req, res) => {
+  Todo.find().then(
+    todos => {
+      res.send({ todos });
+    },
+    err => {
+      res.status(400).send(err);
+    }
+  );
+});
