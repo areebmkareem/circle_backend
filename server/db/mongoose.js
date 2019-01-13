a074c00a2ec68de34d0dc802bb0fc9a899de74ac
flukycoder <flukycoder@gmail.com>
2019-01-13 23:49:29 +0530

    [HEROKU SET UP]

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp");
+mongoose.connect(
+  process.env.MONGOLAB_URI || "mongodb://localhost:27017/TodoApp"
+);
 
 module.exports = {
   mongoose
