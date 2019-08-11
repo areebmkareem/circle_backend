a36833f3b6623ac18095a7e65653002829b85ca4
flukycoder <flukycoder@gmail.com>
2019-08-11 12:33:49 +0530

    merge

--- src/routers/user.js
+++ src/routers/user.js
@@ -4,28 +4,35 @@ const router = new express.Router();
 const circleMethods = require("./circle");
 
 router.post("/register", async (req, res) => {
-  console.log("[/register]", { req });
-  const { name, password, email } = req.body;
-  let user = new User({
-    name,
-    email,
-    password
-  });
-  let data = await user.generateTokenId();
-  circleMethods.createCircle(data.user._id);
-  user.save();
-  res.send({ token: data.token });
+  try {
+    const { name, password, email } = req.body;
+    let user = new User({
+      name,
+      email,
+      password
+    });
+    let data = await user.generateTokenId();
+    circleMethods.createCircle(data.user._id);
+    user.save();
+    res.send({ token: data.token });
+  } catch (error) {
+    res.send({ error });
+  }
 });
 
 router.post("/login", async (req, res) => {
-  let user = new User({});
+  try {
+    let user = new User({});
 
-  const { email, password } = req.body;
-  let response = await User.getCredentials(email, password);
-  if (!response.error) {
-    let data = await user.generateTokenId();
-    res.send({ token: data.token });
-  } else res.send(response);
+    const { email, password } = req.body;
+    let response = await User.getCredentials(email, password);
+    if (!response.error) {
+      let data = await user.generateTokenId();
+      res.send({ token: data.token });
+    } else res.send(response);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 module.exports = router;
