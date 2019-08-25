269f85e2509ace79413f366a4c27ca2965ce276c
shah <muhammedshah@live.in>
2019-08-25 19:59:00 +0530

    Remove existing files. Added gitignore

--- src/db/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.Promise = global.Promise;
-
-mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost:27017/circle");
-
-module.exports = {
-  mongoose
-};
