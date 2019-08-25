629c444ae154294b65218b0938de8e921d156fc4
flukycoder <flukycoder@gmail.com>
2019-08-25 22:25:06 +0530

    Socket -experimental

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 let express = require("express");
 let bodyParser = require("body-parser");
+const path = require("path");
 const userRouter = require("../src/routers/user");
 const Circle = require("./routers/circle");
 const circleRouter = Circle.router;
@@ -9,15 +10,22 @@ let { Todo } = require("./modals/todo");
 let { Users } = require("./modals/user");
 const cors = require("cors");
 const port = process.env.PORT || 3000;
-
+const publicDirectoryPath = path.join(__dirname, "../public");
 const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const socketio = require("socket.io");
+const io = socketio(server);
 
 //setting up route
 app.use(cors());
 app.use(bodyParser.json());
 app.use(userRouter);
 app.use(circleRouter);
+app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Started On Port ${port}`);
 });
+
+io.on("connection", () => console.log("websocket"));
